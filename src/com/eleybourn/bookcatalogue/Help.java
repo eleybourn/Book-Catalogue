8767b71e4566250350cb3c142c842ebb4b479c52
Evan Leybourn <eleybourn@gmail.com>
2010-08-06 13:42:28 +1000

    Help file written

--- src/com/eleybourn/bookcatalogue/Help.java
+++ src/com/eleybourn/bookcatalogue/Help.java
@@ -21,7 +21,14 @@
 package com.eleybourn.bookcatalogue;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * 
@@ -32,6 +39,7 @@ import android.os.Bundle;
  * @author Evan Leybourn
  */
 public class Help extends Activity {
+	public Resources res;
 
 	/**
 	 * Called when the activity is first created. 
@@ -41,6 +49,61 @@ public class Help extends Activity {
 		try {
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.help);
+			res = getResources();
+			
+			TextView nutshell = (TextView) findViewById(R.id.nutshell_link);
+			nutshell.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					String link = res.getString(R.string.help_text_nutshell2);
+					Intent loadweb = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+					startActivity(loadweb); 
+					return;
+				}
+			});
+			
+			TextView zxing = (TextView) findViewById(R.id.barcode_link);
+			zxing.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					Intent marketIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.google.zxing.client.android")); 
+					startActivity(marketIntent);
+					return;
+				}
+			});
+			
+			TextView isbnwiki = (TextView) findViewById(R.id.barcode_link2);
+			isbnwiki.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					String link = res.getString(R.string.help_text_addbooksbarcode4a);
+					Intent loadweb = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+					startActivity(loadweb); 
+					return;
+				}
+			});
+			
+			/* Donation Link */
+			ImageView donate = (ImageView) findViewById(R.id.donate_url);
+			donate.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					Intent loadweb = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=94US4N9MQGDMY&lc=AU&currency_code=AUD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted"));
+					startActivity(loadweb); 
+					return;
+				}
+			});
+			
+			/* Donation Link */
+			TextView amazon = (TextView) findViewById(R.id.amazon_url);
+			amazon.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					Intent loadweb = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.amazon.com/registry/wishlist/27ISBYRXBGXO3/"));
+					startActivity(loadweb); 
+					return;
+				}
+			});
 		} catch (Exception e) {
 			//Log.e("Book Catalogue", "Unknown Exception - BC onCreate - " + e.getMessage() );
 		}

320dd04d1fa2d0fceb1cf4eba575f91891417073
Philip Warner <pjw@rime.com.au>
2014-12-23 21:43:45 +1100

    Added sample AmazonAppKey file

---
+++ src/com/eleybourn/bookcatalogue/amazon/AmazonAppKeyEXAMPLE.java
@@ -0,0 +1,14 @@
+package com.eleybourn.bookcatalogue.amazon;
+
+/**
+ * NOTE: The project must include a class, AmazonAppKey in this folder, which contains a
+ * single static String, KEY, containing the app key granted by Amazon. For testing purposes
+ * this KEY can be junk.
+ * 
+ * If you need to test the app, and do not have an Amazon App Key, just use this file.
+ * 
+ * @author pjw
+ */
+public class AmazonAppKeyEXAMPLE {
+	public static final String KEY = "foo";
+}

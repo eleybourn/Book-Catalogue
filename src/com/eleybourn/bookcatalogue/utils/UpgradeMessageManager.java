3fb77e6d453700539e269f4221f0fc4e04b6a212
Philip Warner <pjw@rime.com.au>
2015-01-02 17:42:50 +1100

    5.1.1 Release changes

--- src/com/eleybourn/bookcatalogue/utils/UpgradeMessageManager.java
+++ src/com/eleybourn/bookcatalogue/utils/UpgradeMessageManager.java
@@ -60,7 +60,7 @@ public class UpgradeMessageManager {
 	.add(152, R.string.new_in_508)
 	.add(154, R.string.new_in_509)
 	.add(162, R.string.new_in_510)
-	.add(164, R.string.new_in_511)
+	.add(166, R.string.new_in_511)
 	;
 
 	

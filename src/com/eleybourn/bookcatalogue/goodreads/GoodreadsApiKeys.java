690cc67ff16b29051698b7c34f99695a065e398d
Nicolas Stouls <nicolas.stouls@insa-lyon.fr>
2017-09-03 14:12:25 +0200

    changing branch

--- src/com/eleybourn/bookcatalogue/goodreads/GoodreadsApiKeys.java
+++
@@ -1,6 +0,0 @@
-package com.eleybourn.bookcatalogue.goodreads;
-
-public class GoodreadsApiKeys {
-	public static final String GOODREADS_DEV_KEY = null;
-	public static final String GOODREADS_DEV_SECRET = null;
-}

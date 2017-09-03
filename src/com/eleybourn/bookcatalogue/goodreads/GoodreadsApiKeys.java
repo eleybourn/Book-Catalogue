3faeae0dcecd21cff7686861216569d8c3862a89
Nicolas Stouls <nicolas.stouls@insa-lyon.fr>
2017-09-03 14:10:39 +0200

    Adding an empty Goodreads api key

---
+++ src/com/eleybourn/bookcatalogue/goodreads/GoodreadsApiKeys.java
@@ -0,0 +1,6 @@
+package com.eleybourn.bookcatalogue.goodreads;
+
+public class GoodreadsApiKeys {
+	public static final String GOODREADS_DEV_KEY = null;
+	public static final String GOODREADS_DEV_SECRET = null;
+}

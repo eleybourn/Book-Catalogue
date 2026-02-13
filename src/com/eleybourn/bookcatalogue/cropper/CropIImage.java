59ea78966ece1644c93a9850b9b221f8439dd7f2
Evan Leybourn <eleybourn@gmail.com>
2026-02-13 17:51:36 +1100

    Fix crop layout for older android versions.

--- src/com/eleybourn/bookcatalogue/cropper/CropIImage.java
+++ src/com/eleybourn/bookcatalogue/cropper/CropIImage.java
@@ -16,18 +16,13 @@
 
 package com.eleybourn.bookcatalogue.cropper;
 
-import java.io.InputStream;
-
 import android.graphics.Bitmap;
-import android.net.Uri;
 
 /**
  * The interface of all images used in gallery.
  */
 public interface CropIImage {
-	int THUMBNAIL_TARGET_SIZE = 320;
-	int MINI_THUMB_TARGET_SIZE = 96;
-	int UNCONSTRAINED = -1;
+    int UNCONSTRAINED = -1;
 
 	/** Get the image list which contains this image. */
     CropIImageList getContainer();
@@ -36,59 +31,15 @@ public interface CropIImage {
     Bitmap fullSizeBitmap(int minSideLength,
                           int maxNumberOfPixels);
 
-	Bitmap fullSizeBitmap(int minSideLength,
-                          int maxNumberOfPixels, boolean rotateAsNeeded);
-
-	Bitmap fullSizeBitmap(int minSideLength,
-                          int maxNumberOfPixels, boolean rotateAsNeeded, boolean useNative);
-
-	int getDegreesRotated();
-
-	boolean ROTATE_AS_NEEDED = true;
-	boolean NO_ROTATE = false;
-	boolean USE_NATIVE = true;
-	boolean NO_NATIVE = false;
-
-	/** Get the input stream associated with a given full size image. */
-    InputStream fullSizeImageData();
-
-	long fullSizeImageId();
-
-	Uri fullSizeImageUri();
-
-	/** Get the path of the (full size) image data. */
-    String getDataPath();
-
-	// Get/Set the title of the image
+    // Get/Set the title of the image
     void setTitle(String name);
 
 	String getTitle();
 
-	// Get metadata of the image
-    long getDateTaken();
-
-	String getMimeType();
-
-	int getWidth();
+    int getWidth();
 
 	int getHeight();
 
 	String getDisplayName();
-
-	// Get property of the image
-    boolean isReadonly();
-
-	boolean isDrm();
-
-	// Get the bitmap/uri of the medium thumbnail
-    Bitmap thumbBitmap(boolean rotateAsNeeded);
-
-	Uri thumbUri();
-
-	// Get the bitmap of the mini thumbnail.
-    Bitmap miniThumbBitmap();
-
-	// Rotate the image
-    boolean rotateImageBy(int degrees);
 
 }
